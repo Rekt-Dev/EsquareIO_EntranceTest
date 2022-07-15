fd678e9435e45ad68bbe81235d8caed86f07ccb7
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-15 20:36:07 +0300

    skeleton

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,12 @@ import { goToAuthor } from "../services/goToAuthor";
 import { goToTitle } from "../services/goToTitle";
 
 const endLink = "/full/843,/0/default.jpg";
-const startLink = "https://www.artic.edu/iiif/2/";
+const startLink = "https://www.googleapis.com/books/v1/volumes?q=";
+let searchQuery = "";
 //the artworkid  which is image_id comes from the json weve just used a promise to get
 const artworkId = "fd26fe20-e8fd-d80a-c3e9-c07365b0db61";
-const constructedLink = startLink + artworkId + endLink;
-const retrieveArtworks =
-  "https://api.artic.edu/api/v1/artworks?fields=id,title,image_id&limit=100";
-console.log(retrieveArtworks);
+const constructedLink = startLink + searchQuery;
+("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id&limit=100");
 export function Card(props) {
   return (
     <MDBRipple
@@ -21,11 +20,12 @@ export function Card(props) {
       <div className="">
         <a href="OpenCard">
           <img
-            width="60%"
-            height="20%"
+            width="90%"
+            height="30%"
             alt=" Book Title"
-            src={constructedLink}
-      
+            src={
+              "https://www.programmableweb.com/sites/default/files/styles/facebook_scale_width_200/public/icon_googledevs_200.png?itok=nHEw_MXV"
+            }
           />
           <div
             className="mask"
